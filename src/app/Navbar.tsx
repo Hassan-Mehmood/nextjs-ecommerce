e229460ad2ef39d530768d5e5950a0991841a936
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-01-30 19:30:18 +0500

    Making the switch from mobile to desktop version of nav

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -4,59 +4,69 @@ import { CgProfile } from 'react-icons/cg';
 import { BsSuitHeart, BsBag } from 'react-icons/bs';
 import { BiSearch } from 'react-icons/bi';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { GrClose } from 'react-icons/gr';
+import { Lato } from '@next/font/google';
 import { useState } from 'react';
+
+const lato = Lato({
+  subsets: [],
+  weight: '400',
+});
 
 const Navbar = () => {
   const [showNavbarMenu, setShowNavbarMenu] = useState(false);
 
   return (
-    <nav className="flex justify-between items-center container mx-auto px-5 min-h-[60px]">
-      <div className="flex ">
+    <nav className="flex justify-between items-center relative mx-auto px-5 min-h-[60px]">
+      <div className="flex">
         <ul
           className={
-            showNavbarMenu === false ? 'hidden' : 'absolute left-0 px-5 top-16 right-0 text-xs'
+            showNavbarMenu === false
+              ? `hidden navBreak:flex  ${lato.className}`
+              : `absolute left-0 px-5 top-16 right-0 text-xs border border-t-black border-b-0 border-l-0 border-r-0  ${lato.className}`
           }
         >
-          {/* absolute left-0 px-5 top-16 right-0 text-xs */}
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">WHAT&apos;S NEW</Link>
           </li>
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">WOMEN</Link>
           </li>
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">DRESSES</Link>
           </li>
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">CLASSICS</Link>
           </li>
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">ATHLETIC</Link>
           </li>
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">CAMPAIGN</Link>
           </li>
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">SUSTAINABLE</Link>
           </li>
-          <li className="py-4 cursor-pointer hover:text-slate-700">
+          <li className="py-3 cursor-pointer hover:text-slate-700">
             <Link href="">JOURNAL</Link>
           </li>
         </ul>
-        <RxHamburgerMenu
-          className="mr-2 cursor-pointer"
-          size={24}
-          onClick={() => setShowNavbarMenu(!showNavbarMenu)}
-        />
-        <BsSuitHeart className="cursor-pointer" size={24} />
+        <div className="navBreak:hidden" onClick={() => setShowNavbarMenu(!showNavbarMenu)}>
+          {showNavbarMenu === false ? (
+            <RxHamburgerMenu className={'mr-2 cursor-pointer'} size={24} />
+          ) : (
+            <GrClose className={'mr-2 cursor-pointer'} size={24} />
+          )}
+        </div>
+        <BsSuitHeart className="cursor-pointer navBreak:hidden" size={24} />
       </div>
       <div>
         <h1 className="text-xl cursor-pointer">BOHEMIAN TRADERS</h1>
       </div>
       <div className="flex">
         <CgProfile size={24} className="mr-2 cursor-pointer" />
-        <BsSuitHeart className="hidden cursor-pointer" />
-        <BsBag className="hidden cursor-pointer" />
+        <BsSuitHeart className="hidden navBreak:block mr-2 cursor-pointer" size={24} />
+        <BsBag className="hidden navBreak:block mr-2 cursor-pointer" size={24} />
         <BiSearch className="cursor-pointer" size={24} />
       </div>
     </nav>
