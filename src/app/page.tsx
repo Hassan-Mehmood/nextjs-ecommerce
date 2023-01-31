7f7308bebd28fbf4666aa4e55496791d4e02a5ed
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-01-31 15:02:50 +0500

    Main section done Testing needed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="px-2 mx-auto">
+      <section className="cursor-pointer">
+        <Image
+          alt=""
+          src="/denim-hero-banner.webp"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="hidden md:block w-full h-auto"
+        />
+        <Image
+          alt=""
+          src="/denim-hero-banner-phone.webp"
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="block md:hidden w-full max-h-[518px] object-cover"
+        />
+      </section>
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   theme: {
     screens: {
       navBreak: '960px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     extend: {},
   },
