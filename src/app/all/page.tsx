36105ae2cc6a531b76b93e2481245ea53ade0ee8
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-02-10 19:50:40 +0500

    Working on single product page this page will load for to show every single product

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -1,20 +1,18 @@
-"use client";
-import React, { useState } from "react";
-import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
-import { ProductGrid } from "./components/ProductGrid";
-import { RefineSpec } from "./components/RefineSpec";
-import { SortSpec } from "./components/SortSpec";
+'use client';
+import { useState } from 'react';
+import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
+import { ProductGrid } from './components/ProductGrid';
+import { RefineSpec } from './components/RefineSpec';
+import { SortSpec } from './components/SortSpec';
 
-export default function all() {
+export default function All() {
   const [refineSection, setRefineSection] = useState(false);
   const [sortSection, setSortSection] = useState(false);
-  
+
   return (
     <section className="mx-4 flex flex-col lg:flex-row lg:mt-10 tracking-wider space-x-14 px-20">
       <aside className="mt-20 lg:mt-0">
-        <h1 className=" text-2xl font-light text-center pb-6 lg:text-3xl lg:text-left">
-          CLASSICS
-        </h1>
+        <h1 className=" text-2xl font-light text-center pb-6 lg:text-3xl lg:text-left">CLASSICS</h1>
         <section className="grid grid-cols-2 border border-black lg:hidden">
           <div
             className="flex justify-between items-center border-r border-black py-3 px-4 cursor-pointer"
