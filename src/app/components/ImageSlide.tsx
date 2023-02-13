4cb4c6b73246a1f72a5f875d8b09755ed0ea0d67
Hassan-Mehmood <hassanafridi1913@gmail.com>
2023-02-13 20:42:33 +0500

    Working on the carousel

--- src/app/components/ImageSlide.tsx
+++ src/app/components/ImageSlide.tsx
@@ -1,25 +1,48 @@
-"use client";
-import { FaFacebookF, FaInstagram } from "react-icons/fa";
-import {
-  BsTwitter,
-  BsPinterest,
-  BsChevronLeft,
-  BsChevronRight,
-} from "react-icons/bs";
-import { imageSlideData } from "@/JSONData/imageSlideData";
-import React, { useState } from "react";
-import { MdEmail } from "react-icons/md";
-import { BiLink } from "react-icons/bi";
-import { GrClose } from "react-icons/gr";
-import Image from "next/image";
+'use client';
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { BsTwitter, BsPinterest, BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import { imageSlideData } from '@/JSONData/imageSlideData';
+import React, { useState } from 'react';
+import { MdEmail } from 'react-icons/md';
+import { BiLink } from 'react-icons/bi';
+import { GrClose } from 'react-icons/gr';
+import Image from 'next/image';
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 6,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 export const ImageSlide = () => {
-  const [banner, setBanner] = useState(false);
+  const [showBanner, setBanner] = useState(false);
+  const [showBannerID, setShowBannerID] = useState<number | undefined>(undefined);
   const [currentCarousel, setCurrentCarousel] = useState(0);
 
+  function showImageBanner(imageID: number) {
+    imageSlideData[imageID - 1].visible = true;
+    setBanner(true);
+    setShowBannerID(imageID - 1);
+  }
+
   return (
-    <div className="relative flex">
-      <button
+    <section className="relative flex mb-4">
+      {/* <button
         className="absolute top-0 bottom-0 m-auto w-9 md:w-12 lg:w-16 opacity-0
        z-10 hover:bg-fadedWhite hover:opacity-100 flex justify-center items-center
        transition-opacity duration-300 cursor-pointer"
@@ -28,20 +51,21 @@ export const ImageSlide = () => {
       >
         <BsChevronLeft
           className={`${
-            currentCarousel <= 0 ? "text-gray-500" : "text-black"
+            currentCarousel <= 0 ? 'text-gray-500' : 'text-black'
           } text-sm md:text-xl lg:text-3xl stroke-1`}
         />
-      </button>
-      {imageSlideData
-        .slice(currentCarousel, currentCarousel + 6)
-        .map((imageElement, primeIndex) => {
-          return (
-            <div key={primeIndex}>
+      </button> */}
+
+      <Carousel responsive={responsive} className="w-full">
+        {imageSlideData
+          // .slice(currentCarousel, currentCarousel + 6)
+          .map((imageElement) => (
+            <div key={imageElement.id}>
               <div className="relative">
                 <Image
                   src={imageElement.source}
                   alt="model"
-                  className="w-64"
+                  className="w-full"
                   width={0}
                   height={0}
                   sizes="100vw"
@@ -51,14 +75,18 @@ export const ImageSlide = () => {
                   duration-300 hover:bg-fadedBlack absolute w-full h-full top-0 
                   flex justify-center items-center cursor-pointer"
                   onClick={() => {
-                    setBanner(true);
-                    imageElement.visible = true;
+                    showImageBanner(imageElement.id);
+                    // setBanner(true);
+                    // imageElement.visible = true;
                   }}
                 >
                   <FaInstagram className="text-base lg:text-3xl text-white" />
                 </div>
               </div>
-              {banner && imageElement.visible == true && (
+            </div>
+          ))}
+      </Carousel>
+      {/* {banner && imageElement.visible == true && (
                 <div className="fixed w-screen h-screen top-0 left-0 z-20 bg-fadedMaroon">
                   <div
                     className="absolute flex flex-col bg-white top-56 md:top-40 lg:top-50 
@@ -80,10 +108,7 @@ export const ImageSlide = () => {
                       className="w-full md:w-349 md:min-h-300 lg:min-h-0 lg:w-full"
                       sizes="100vw"
                     />
-                    <div
-                      className="text-center px-3 flex items-center flex-col mt-4
-                    lg:px-20 md:mt-0 lg:w-cardWidth"
-                    >
+                    <div className="text-center px-3 flex items-center flex-col mt-4 lg:px-20 md:mt-0 lg:w-cardWidth">
                       <h1 className="text-xs md:text-sm lg:text-base mt-1 md:mt-3 lg:mt-16">
                         {imageElement.description}
                       </h1>
@@ -105,11 +130,8 @@ export const ImageSlide = () => {
                     </div>
                   </div>
                 </div>
-              )}
-            </div>
-          );
-        })}
-      <button
+              )} */}
+      {/* <button
         className="absolute top-0 bottom-0 right-0 w-9 md:w-12 lg:w-16 m-auto
        z-10 hover:bg-fadedWhite hover:opacity-100 flex justify-center items-center
        opacity-0 transition-opacity duration-300 cursor-pointer"
@@ -118,12 +140,10 @@ export const ImageSlide = () => {
       >
         <BsChevronRight
           className={`${
-            currentCarousel + 6 >= imageSlideData.length
-              ? "text-gray-500"
-              : "text-black"
+            currentCarousel + 6 >= imageSlideData.length ? 'text-gray-500' : 'text-black'
           } text-sm md:text-xl lg:text-3xl stroke-1`}
         />
-      </button>
-    </div>
+      </button> */}
+    </section>
   );
 };
